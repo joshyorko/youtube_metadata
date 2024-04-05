1c1c575e9ba69099269d79cf6cc1b75fd89810f1
Josh Yorko <joshua.yorko@gainwelltechnologies.com>
2024-04-05 07:31:34 -0400

    Refactor import statements and update variable names in transcribe.py

--- youtube-automation-tools/services/transcribe_service/app/services/transcribe.py
+++ youtube-automation-tools/services/transcribe_service/app/services/transcribe.py
@@ -1,5 +1,9 @@
 from fastapi import UploadFile
-from ..models import TranscriptionResult, Segment, TranscriptionInfo  # Adjust the import path
+from ..models import (
+    TranscriptionResult,
+    Segment,
+    TranscriptionInfo,
+)  # Adjust the import path
 import whisper
 import asyncio
 import aiofiles
@@ -16,7 +20,7 @@ async def transcribe_audio_file(file: UploadFile) -> TranscriptionResult:
     temp_file_path = temp_file.name
     temp_file.close()  # Close the file to ensure it's not locked
 
-    async with aiofiles.open(temp_file_path, 'wb') as temp_aio_file:
+    async with aiofiles.open(temp_file_path, "wb") as temp_aio_file:
         await file.seek(0)  # Reset file pointer to the beginning
         content = await file.read()  # Read file content asynchronously
         await temp_aio_file.write(content)  # Write content to temporary file
@@ -24,23 +28,29 @@ async def transcribe_audio_file(file: UploadFile) -> TranscriptionResult:
     # Perform transcription using Whisper in an asynchronous thread
     try:
         result = await asyncio.to_thread(model.transcribe, temp_file_path)
-        print(result['segments'])
-        
-        segments = [Segment(
-            start=0,  # Placeholder start time
-            end=result.get("duration", 0),  # Use Whisper's reported duration
-            text=result["text"],  # The transcribed text
-        )]
-        
+        print(result["segments"])
+
+        segments = [
+            Segment(
+                start=0,  # Placeholder start time
+                end=result.get("duration", 0),  # Use Whisper's reported duration
+                text=result["text"],  # The transcribed text
+            )
+        ]
+
         info = TranscriptionInfo(
             language=result.get("language", "unknown"),  # Detected language
-            language_probability=result.get("language_probability", 1.0),  # Placeholder probability
+            language_probability=result.get(
+                "language_probability", 1.0
+            ),  # Placeholder probability
             duration=result.get("duration"),  # Audio duration
-            translation=result.get("translation")  # Translated text, if any
+            translation=result.get("translation"),  # Translated text, if any
         )
-        
+
         transcription_result = TranscriptionResult(segments=segments, info=info)
     finally:
-        os.remove(temp_file_path)  # Ensure the temporary file is deleted after processing
+        os.remove(
+            temp_file_path
+        )  # Ensure the temporary file is deleted after processing
 
     return transcription_result
